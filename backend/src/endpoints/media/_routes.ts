89a7e7fade6649ea1e717c12ae6f1ec237376bef
idaohlen <ida-alexandra.ohlen@chasacademy.se>
2026-06-05 15:13:06 +0200

    feat: Add rate limit middleware

--- backend/src/endpoints/media/_routes.ts
+++ backend/src/endpoints/media/_routes.ts
@@ -3,10 +3,11 @@ import { mediaGet } from './index.get.js';
 import { mediaPost } from './index.post.js';
 import { mediaDelete } from './[id].delete.js';
 import { mediaPatch } from './[id].patch.js';
+import { uploadRateLimit } from '../../lib/uploadRateLimit.js';
 
 const mediaRouter = new Hono()
   .get('/', ...mediaGet)
-  .post('/', ...mediaPost)
+  .post('/', uploadRateLimit, ...mediaPost)
   .patch('/:id', ...mediaPatch)
   .delete('/:id', ...mediaDelete);
 
